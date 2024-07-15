a9b11ce6e7624dd4119c1d96c4527399c62b5938
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2024-07-15 10:13:49 +0100

    NP-111: changed auth payload

--- src/test/scala/uk/gov/hmrc/api/service/AuthService.scala
+++ src/test/scala/uk/gov/hmrc/api/service/AuthService.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2024 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.api.service
@@ -18,28 +29,11 @@ class AuthService extends HttpClient {
   val authPayload: String =
     s"""
        |{
-       |  "clientId": "id-123232",
-       |  "authProvider": "PrivilegedApplication",
-       |  "applicationId":"app-1",
-       |  "applicationName": "App 1",
-       |  "enrolments": ["read:individuals-matching",
-       |  "read:individuals-income",
-       |  "read:individuals-income-sa",
-       |  "read:individuals-income-sa-additional-information",
-       |  "read:individuals-income-sa-employments",
-       |  "read:individuals-income-sa-foreign",
-       |  "read:individuals-income-sa-interests-and-dividends",
-       |  "read:individuals-income-sa-partnerships",
-       |  "read:individuals-income-sa-pensions-and-state-benefits",
-       |  "read:individuals-income-sa-other",
-       |  "read:individuals-income-sa-self-employments",
-       |  "read:individuals-income-sa-source",
-       |  "read:individuals-income-sa-summary",
-       |  "read:individuals-income-sa-trusts",
-       |  "read:individuals-income-sa-uk-properties",
-       |  "read:individuals-income-paye",
-       |  "read:individuals-employments",
-       |  "read:individuals-employments-paye"],
+       |  "clientId": "uknw-auth-checker-api",
+       |  "authProvider": "StandardApplication",
+       |  "applicationId":"uknw-auth-checker-api",
+       |  "applicationName": "uknw-auth-checker-api",
+       |  "enrolments": [],
        |  "ttl": 5000
        |}
      """.stripMargin
