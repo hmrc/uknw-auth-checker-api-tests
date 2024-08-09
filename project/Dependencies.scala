4ec3b4b298a1ba5bfb3f62c61ff4178dbf9a4523
scrumconnect <alessandro5cinque@gmail.com>
2024-08-09 14:39:03 +0100

    NP-262 Upgrading Dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,11 +4,11 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "com.typesafe"         % "config"                  % "1.4.3"  % Test,
-    "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.2.7"  % Test,
-    "com.typesafe.play"   %% "play-ws-standalone-json" % "2.2.7"  % Test,
+    "org.playframework"   %% "play-ahc-ws-standalone"  % "3.1.0-M2"  % Test,
+    "org.playframework"   %% "play-ws-standalone-json" % "3.1.0-M2"  % Test,
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8" % Test,
-    "org.scalatest"       %% "scalatest"               % "3.2.18" % Test,
-    "org.slf4j"            % "slf4j-simple"            % "2.0.13" % Test
+    "org.scalatest"       %% "scalatest"               % "3.3.0-SNAP4" % Test,
+    "org.slf4j"            % "slf4j-simple"            % "2.0.15" % Test
   )
 
 }
