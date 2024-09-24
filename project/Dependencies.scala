257cd74a44b1ced43b15804f764690370526b873
scrumconnect <alessandro5cinque@gmail.com>
2024-09-24 15:56:40 +0100

    NP-335 correction

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     "com.vladsch.flexmark"  % "flexmark-all"            % "0.64.8"         % Test,
     "org.scalatest"        %% "scalatest"               % "3.3.0-SNAP4"    % Test,
     "org.slf4j"             % "slf4j-simple"            % "2.0.16"         % Test,
-    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.2.0"          % Test,
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"          % Test,
     "org.scalacheck"       %% "scalacheck"              % "1.18.1"         % Test,
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion % Test
   )
