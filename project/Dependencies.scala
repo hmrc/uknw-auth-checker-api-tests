79f8d74baaea8359116bfa0917a3dd62c70a015b
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2024-11-07 13:24:36 +0000

    NP-Packages: Updated packages (#48)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,12 +1,13 @@
 import sbt.*
 
 object Dependencies {
-  private val bootstrapVersion = "9.4.0"
+  private val bootstrapVersion = "9.5.0"
+
   val test: Seq[ModuleID]      = Seq(
-    "org.commonmark"        % "commonmark"              % "0.23.0"         % Test,
+    "org.commonmark"        % "commonmark"              % "0.24.0"         % Test,
     "com.typesafe"          % "config"                  % "1.4.3"          % Test,
     "org.playframework"    %% "play-ahc-ws-standalone"  % "3.0.5"          % Test,
-    "org.playframework"    %% "play-ws-standalone-json" % "3.1.0-M1"       % Test,
+    "org.playframework"    %% "play-ws-standalone-json" % "3.1.0-M2"       % Test,
     "com.vladsch.flexmark"  % "flexmark-all"            % "0.64.8"         % Test,
     "org.scalatest"        %% "scalatest"               % "3.3.0-SNAP4"    % Test,
     "org.slf4j"             % "slf4j-simple"            % "2.0.16"         % Test,
