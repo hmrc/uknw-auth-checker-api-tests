32cdaeb16fd19832ac1f5cdcf3fd19c58a4fe8df
Sinimol Babu <22820561+sini-george@users.noreply.github.com>
2025-04-23 11:35:48 +0100

    NP-346: Updates dependencies to use api-test-runner (#60)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,16 +4,11 @@ object Dependencies {
   private val bootstrapVersion = "9.11.0"
 
   val test: Seq[ModuleID] = Seq(
-    "org.commonmark"        % "commonmark"              % "0.24.0"         % Test,
-    "com.typesafe"          % "config"                  % "1.4.3"          % Test,
-    "org.playframework"    %% "play-ahc-ws-standalone"  % "3.0.7"          % Test,
-    "org.playframework"    %% "play-ws-standalone-json" % "3.1.0-M5"       % Test,
-    "com.vladsch.flexmark"  % "flexmark-all"            % "0.64.8"         % Test,
-    "org.scalatest"        %% "scalatest"               % "3.2.19"         % Test,
-    "org.slf4j"             % "slf4j-simple"            % "2.0.17"         % Test,
-    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"          % Test,
-    "org.scalacheck"       %% "scalacheck"              % "1.18.1"         % Test,
-    "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion % Test
+    "org.commonmark"        % "commonmark"             % "0.24.0"         % Test,
+    "uk.gov.hmrc"          %% "api-test-runner"        % "0.9.0"          % Test,
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"          % Test,
+    "org.scalacheck"       %% "scalacheck"             % "1.18.1"         % Test,
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapVersion % Test
   )
 
 }
