b3c0e7f8d54d334194b50858bbd89c5ea0d9effc
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2025-05-20 14:20:15 +0100

    NP-370: Updated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,11 @@
 import sbt.*
 
 object Dependencies {
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.12.0"
 
   val test: Seq[ModuleID] = Seq(
     "org.commonmark"        % "commonmark"             % "0.24.0"         % Test,
-    "uk.gov.hmrc"          %% "api-test-runner"        % "0.9.0"          % Test,
+    "uk.gov.hmrc"          %% "api-test-runner"        % "0.10.0"          % Test,
     "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"          % Test,
     "org.scalacheck"       %% "scalacheck"             % "1.18.1"         % Test,
     "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapVersion % Test
