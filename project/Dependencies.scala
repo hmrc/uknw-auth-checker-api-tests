b6505698f7f91859c86f09e5cf1149a06b2733de
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2025-07-23 11:50:22 +0100

    NP-386: Upgraded bootstrap

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,10 @@
 import sbt.*
 
 object Dependencies {
-  private val bootstrapVersion = "9.15.0"
+  private val bootstrapVersion = "9.18.0"
 
   val test: Seq[ModuleID] = Seq(
-    "org.commonmark"        % "commonmark"             % "0.24.0"         % Test,
+    "org.commonmark"        % "commonmark"             % "0.25.0"         % Test,
     "uk.gov.hmrc"          %% "api-test-runner"        % "0.10.0"         % Test,
     "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"          % Test,
     "org.scalacheck"       %% "scalacheck"             % "1.18.1"         % Test,
